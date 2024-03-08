b60609d1d0057f1e673b8a5e56a2b83d66f043e5
Ophiltry <46213354+Ophiltry@users.noreply.github.com>
2024-03-08 10:11:03 -0500

    Update import_json.py

--- import_json.py
+++ import_json.py
@@ -3,11 +3,12 @@ import json
 with open("test2.json", "r") as f:
     data = json.load(f)
 
-print(data["name"])
+#print(data["name"])
 
-st.write(data["name"])
+#st.write(data["name"])
 
 
+st.write("blah")
 
 
 
