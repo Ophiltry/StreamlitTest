9854d8127f54d7931bc02ddfc31ec45c49c7a65c
Ophiltry <46213354+Ophiltry@users.noreply.github.com>
2024-03-08 10:14:16 -0500

    Update import_json.py

--- import_json.py
+++ import_json.py
@@ -1,11 +1,13 @@
 import json
+import streamlit as st 
+import pandas as pd
 
 with open("test2.json", "r") as f:
     data = json.load(f)
 
 #print(data["name"])
 
-#st.write(data["name"])
+st.write(data["name"])
 
 
 st.write("blah")
